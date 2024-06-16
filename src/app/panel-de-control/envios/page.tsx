4b3e0b44210c11b5073aa483138a68f82025fa52
JereUR <jeremias.jdv@gmail.com>
2024-06-15 22:39:04 -0300

    feat: Different pages created. Not setted (default)

---
+++ src/app/panel-de-control/envios/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ShipmentsPage = () => {
+  return <div>ShipmentsPage</div>
+}
+
+export default ShipmentsPage
---
+++ src/app/panel-de-control/registros/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RecordsPage = () => {
+  return <div>RecordsPage</div>
+}
+
+export default RecordsPage
