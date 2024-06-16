5414a894b987974c764c7f5117f541d744d83b38
JereUR <jeremias.jdv@gmail.com>
2024-06-15 22:37:43 -0300

    feat: SIntaxis change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
 const config = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
