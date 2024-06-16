4675cb596cc44c0efff2a3879c5144e9450fc4bd
JereUR <jeremias.jdv@gmail.com>
2024-06-15 22:37:06 -0300

    feat: Redirect to /panel-de-control when access to /

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/panel-de-control',
+        permanent: true
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
