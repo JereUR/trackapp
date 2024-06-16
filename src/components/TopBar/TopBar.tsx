a5c6dc61c65e449e6b644bf47f7d15da20bc979d
JereUR <jeremias.jdv@gmail.com>
2024-06-16 05:43:16 -0300

    feat: Dark tag for dark theme styles used

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,15 +1,8 @@
-import { useTheme } from 'next-themes'
 import ThemeSwitcher from '../global/ThemeSwitcher'
 
 const TopBar = () => {
-  const { theme } = useTheme()
-
   return (
-    <div
-      className={`flex justify-between items-center p-6 text-white ${
-        theme === 'dark' ? 'bg-slate-800' : 'bg-slate-600'
-      }`}
-    >
+    <div className="flex justify-between items-center p-6 text-white bg-slate-600 dark:bg-slate-800">
       <p className="text-3xl font-semibold">TrackApp</p>
       <ThemeSwitcher />
     </div>
--- src/components/TopBar/TopBarWithLinks.tsx
+++ src/components/TopBar/TopBarWithLinks.tsx
@@ -1,33 +1,16 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 
 import ThemeSwitcher from '@components/global/ThemeSwitcher'
-import { useTheme } from 'next-themes'
 
 const TopBarWithLinks = () => {
   const pathname = usePathname()
   const router = useRouter()
-  const [previousClassName, setPreviousClassName] =
-    useState<string>('bg-slate-800')
-  const { theme, resolvedTheme } = useTheme()
-
-  useEffect(() => {
-    if (theme === 'light') {
-      setPreviousClassName('bg-slate-600')
-    } else {
-      if (theme === 'dark') {
-        setPreviousClassName('bg-slate-800')
-      }
-    }
-  }, [theme])
 
   return (
-    <div
-      className={`flex justify-between items-center p-6 text-white ${previousClassName}`}
-    >
+    <div className="flex justify-between items-center p-6 text-white bg-slate-600 dark:bg-slate-800">
       <div className="flex gap-16 items-center">
         <div>
           <p
@@ -40,32 +23,32 @@ const TopBarWithLinks = () => {
         <div className="flex space-x-6 ">
           <Link
             href={'/panel-de-control'}
-            className={`hover:bg-purple-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
-              pathname === '/panel-de-control' && 'bg-purple-800'
+            className={`hover:bg-blue-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
+              pathname === '/panel-de-control' && 'bg-blue-800'
             }`}
           >
             Inicio
           </Link>
           <Link
             href={'/panel-de-control/envios'}
-            className={`hover:bg-purple-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
-              pathname === '/panel-de-control/envios' && 'bg-purple-800'
+            className={`hover:bg-blue-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
+              pathname === '/panel-de-control/envios' && 'bg-blue-800'
             }`}
           >
             Envios
           </Link>
           <Link
             href={'/panel-de-control/registros'}
-            className={`hover:bg-purple-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
-              pathname === '/panel-de-control/registros' && 'bg-purple-800'
+            className={`hover:bg-blue-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
+              pathname === '/panel-de-control/registros' && 'bg-blue-800'
             }`}
           >
             Registros
           </Link>
           <Link
             href={'/panel-de-control/usuarios'}
-            className={`hover:bg-purple-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
-              pathname === '/panel-de-control/usuarios' && 'bg-purple-800'
+            className={`hover:bg-blue-800 hover:opacity-70 px-4 py-1 rounded-lg transition duration-300 ${
+              pathname === '/panel-de-control/usuarios' && 'bg-blue-800'
             }`}
           >
             Usuarios
