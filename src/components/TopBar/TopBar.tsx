08b6d9821d0159a820bb5f740077facf6588236d
JereUR <jeremias.jdv@gmail.com>
2024-06-16 21:47:51 -0300

    feat: Components setted to function

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,6 +1,6 @@
 import ThemeSwitcher from '../global/ThemeSwitcher'
 
-const TopBar = () => {
+export default function TopBar() {
   return (
     <div className="flex justify-between items-center p-6 text-white shadow-md bg-slate-600 dark:bg-slate-800">
       <p className="text-3xl font-semibold">TrackApp</p>
@@ -8,5 +8,3 @@ const TopBar = () => {
     </div>
   )
 }
-
-export default TopBar
--- src/components/TopBar/TopBarWithLinks.tsx
+++ src/components/TopBar/TopBarWithLinks.tsx
@@ -3,9 +3,9 @@
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 
-import ThemeSwitcher from '@components/global/ThemeSwitcher'
+import ThemeSwitcher from '@/components/global/ThemeSwitcher'
 
-const TopBarWithLinks = () => {
+export default function TopBarWithLinks() {
   const pathname = usePathname()
   const router = useRouter()
 
@@ -59,5 +59,3 @@ const TopBarWithLinks = () => {
     </div>
   )
 }
-
-export default TopBarWithLinks
