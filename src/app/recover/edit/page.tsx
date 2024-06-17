4d0d0eea06bbfb1a1e1287642ea7596a606e2a23
JereUR <jeremias.jdv@gmail.com>
2024-06-16 22:05:52 -0300

    feat: Recover page wraped in Suspense

--- src/app/recover/edit/page.tsx
+++ src/app/recover/edit/page.tsx
@@ -1,12 +1,18 @@
+import Loader from '@/components/Loader'
 import RecoverForm from '@/components/session/RecoverForm'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'TrackApp - Recuperar contraseña'
 }
 
 const RecoverPage = () => {
-  return <RecoverForm />
+  return (
+    <Suspense fallback={<Loader />}>
+      <RecoverForm />
+    </Suspense>
+  )
 }
 
 export default RecoverPage
