db2d73f047a7afef2fbff3ad684026d493905233
JereUR <jeremias.jdv@gmail.com>
2024-06-16 21:44:22 -0300

    fix: Imports fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import { Toaster } from '@/components/ui/toaster'
 import { usePathname } from 'next/navigation'
 
-import TopBar from '@components/topbar/TopBar'
-import TopBarWithLinks from '@components/topbar/TopBarWithLinks'
-import ThemeProvider from '@components/providers/ThemeProvider'
-import AuthContextProvider from '@components/context/AuthContext'
+import AuthContextProvider from '@/components/context/AuthContext'
+import ThemeProvider from '@/components/providers/ThemeProvider'
+import TopBar from '@/components/topbar/TopBar'
+import TopBarWithLinks from '@/components/topbar/TopBarWithLinks'
+import { Toaster } from '@/components/ui/toaster'
 
 const fontSans = FontSans({
   subsets: ['latin'],
