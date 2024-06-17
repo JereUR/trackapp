ca9708678497ce6bb830ee5408a27cfbbfc9e59e
JereUR <jeremias.jdv@gmail.com>
2024-06-16 21:36:58 -0300

    feat: Interfaces and initial states for recover and register actions

--- src/components/types/User.ts
+++ src/components/types/User.ts
@@ -13,18 +13,108 @@ export type User = {
 export interface Login {
   email: string
   password: string
-  remember_me: boolean
+  remember_me?: boolean
 }
 
-export const initialData = {
+export const initialData: Login = {
   email: '',
   password: '',
   remember_me: false
 }
 
-export const initialFocus = { email: false, password: false }
-
-export interface FocusState {
+export interface FocusStateLogin {
   email: boolean
   password: boolean
+}
+
+export const initialFocusLogin: FocusStateLogin = {
+  email: false,
+  password: false
+}
+
+export interface FormErrorsLogin {
+  email?: string
+  password?: string
+  [key: string]: string | undefined
+}
+
+export const initialErrors: FormErrorsLogin = {
+  email: '',
+  password: ''
+}
+
+/* Register info */
+
+export interface Register {
+  first_name: string
+  last_name: string
+  email: string
+  gender: string
+  date: string
+  password: string
+  password_confirmation: string
+}
+
+export const initialDataRegister: Register = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  gender: '',
+  date: '',
+  password: '',
+  password_confirmation: ''
+}
+
+export interface FormErrorsRegister {
+  first_name?: string
+  last_name?: string
+  email?: string
+  gender?: string
+  date?: string
+  password?: string
+  password_confirmation?: string
+  [key: string]: string | undefined
+}
+
+export const initialErrorsRegister: FormErrorsRegister = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  gender: '',
+  date: '',
+  password: '',
+  password_confirmation: ''
+}
+
+/* Recover info */
+
+export interface Recover {
+  password: string
+  confirm_password: string
+}
+
+export const initialDataRecover: Recover = {
+  password: '',
+  confirm_password: ''
+}
+
+export interface FormErrorsRecover {
+  password?: string
+  confirm_password?: string
+  [key: string]: string | undefined
+}
+
+export const initialErrorsRecover: FormErrorsRecover = {
+  password: '',
+  confirm_password: ''
+}
+
+export interface FocusStateRecover {
+  password: boolean
+  confirm_password: boolean
+}
+
+export const initialFocusRecover: FocusStateRecover = {
+  password: false,
+  confirm_password: false
 }
