036a73dc13f65bf851c911ea67ade26d416e0707
JereUR <jeremias.jdv@gmail.com>
2024-06-22 20:24:38 -0300

    feat: Aside section moved to another component

--- src/components/dashboard/ShowFleets.tsx
+++ src/components/dashboard/ShowFleets.tsx
@@ -14,20 +14,16 @@ const ShowFleets = () => {
   }, [token])
 
   return (
-    <div className="flex gap-8 m-10">
-      <div className=" w-3/4 flex flex-col gap-8">
-        <p className="text-3xl font-bold">Mis flotas</p>
+    <div className="flex flex-col gap-8">
+      <p className="text-3xl font-bold">Mis flotas</p>
 
-        <div className="justify-around">
-          {fleets.length > 0 ? (
-            fleets.map((fleet) => <FleetItem key={fleet.id} fleet={fleet} />)
-          ) : (
-            <p>Sin flotas disponibles</p>
-          )}
-        </div>
+      <div className="justify-around">
+        {fleets.length > 0 ? (
+          fleets.map((fleet) => <FleetItem key={fleet.id} fleet={fleet} />)
+        ) : (
+          <p>Sin flotas disponibles</p>
+        )}
       </div>
-
-      <div className="w-1/4">Aside</div>
     </div>
   )
 }
