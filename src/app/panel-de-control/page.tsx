2d1dea039101bed9d091548fd4f24dccffbe3086
JereUR <jeremias.jdv@gmail.com>
2024-06-22 20:24:17 -0300

    feat: Home page for admin and staff users updated

--- src/app/panel-de-control/page.tsx
+++ src/app/panel-de-control/page.tsx
@@ -1,10 +1,16 @@
+import AsideSection from '@/components/dashboard/AsideSection'
 import ShowFleets from '@/components/dashboard/ShowFleets'
 import React from 'react'
 
 const AdminHomePage = () => {
   return (
-    <div>
-      <ShowFleets />
+    <div className="flex gap-8 m-10">
+      <div className="w-3/4">
+        <ShowFleets />
+      </div>
+      <div className="w-1/4">
+        <AsideSection />
+      </div>
     </div>
   )
 }
