cb8dbd2628cd4af9db02ff1c9d75d609e547d3b4
JereUR <jeremias.jdv@gmail.com>
2024-06-25 22:26:19 -0300

    feat: Styles updated to button section

--- src/components/dashboard/maps/ModalMap.jsx
+++ src/components/dashboard/maps/ModalMap.jsx
@@ -86,10 +86,10 @@ const ModalMap = ({ shipment, onClose }) => {
       className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
     >
       <div className="bg-white dark:bg-gray-800 p-2 md:p-4 m-2 md:m-8 rounded shadow-lg w-full max-w-lg mx-2 z-50">
-        <div className="flex justify-end">
+        <div className="flex justify-end mb-2">
           <button
             onClick={onClose}
-            className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 transition-colors p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-600"
+            className="bg-gray-700 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 transition-colors p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <IoContract size={24} />
           </button>
