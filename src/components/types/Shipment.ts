ef8d6ebaf3c9679c9d639ec950ff27998cb6c72c
JereUR <jeremias.jdv@gmail.com>
2024-06-27 22:42:26 -0300

    feat: Type and const for calendar added

--- src/components/types/Shipment.ts
+++ src/components/types/Shipment.ts
@@ -25,6 +25,19 @@ export type Shipment = {
   actual_position?: { lat: number; lng: number; time: string }
 }
 
+export type ShipmentItem = {
+  id: number
+  fleet_id: number
+  name: string
+  time_start: string
+  time_end: string
+}
+
+export type ShipmentGroup = {
+  date: string
+  shipments: ShipmentItem[]
+}
+
 export interface PropsAddShipment {
   id?: number
   fleet_id?: number
@@ -44,4 +57,37 @@ export const posibleStatus = [
   'Completado',
   'Rechazado',
   'Cancelado'
+]
+
+export const times = [
+  '07:30',
+  '07:45',
+  '08:00',
+  '08:15',
+  '08:30',
+  '08:45',
+  '09:00',
+  '09:15',
+  '09:30',
+  '09:45',
+  '10:00',
+  '10:15',
+  '10:30',
+  '19:30',
+  '19:45',
+  '20:00',
+  '20:15',
+  '20:30',
+  '20:45',
+  '21:00',
+  '21:15',
+  '21:30',
+  '21:45',
+  '22:00',
+  '22:15',
+  '22:30',
+  '22:45',
+  '23:00',
+  '23:15',
+  '23:30'
 ]
