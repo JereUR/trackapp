5d63214694a93ff28854faab7d67fa95ccc5c1c6
JereUR <jeremias.jdv@gmail.com>
2024-06-29 02:51:25 -0300

    fix: Created page

---
+++ src/app/panel-de-control/envios/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'TrackApp - Vista envío'
+}
+
+const ShipmentPage = () => {
+  return <div>ShipmentPage</div>
+}
+
+export default ShipmentPage
---
+++ src/app/panel-de-control/envios/editar/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'TrackApp - Editar envío'
+}
+
+const EditShipmentPage = () => {
+  return <div>EditShipmentPage</div>
+}
+
+export default EditShipmentPage
