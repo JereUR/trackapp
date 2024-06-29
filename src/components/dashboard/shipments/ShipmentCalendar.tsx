59a619f0be68bf76cb1cb8acde2b89e425b2b89e
JereUR <jeremias.jdv@gmail.com>
2024-06-29 02:47:51 -0300

    feat: Button to add shipment added

--- src/components/dashboard/shipments/ShipmentCalendar.tsx
+++ src/components/dashboard/shipments/ShipmentCalendar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { ShipmentGroup } from '@/components/types/Shipment'
 import Calendar from './Calendar'
-import FleetLegend from './FleetLegend'
 
 interface ShipmentCalendarProps {
   shipments: ShipmentGroup[]
@@ -11,7 +10,6 @@ interface ShipmentCalendarProps {
 const ShipmentCalendar: React.FC<ShipmentCalendarProps> = ({ shipments }) => {
   return (
     <div>
-      <FleetLegend />
       {shipments.map((shipmentGroup) => (
         <div key={shipmentGroup.date}>
           <Calendar shipmentGroup={shipmentGroup} />
