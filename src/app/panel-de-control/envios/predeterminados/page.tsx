85e22d0d68bfe9485f802fa991411d6d5533b396
JereUR <jeremias.jdv@gmail.com>
2024-07-02 22:02:56 -0300

    feat: Custom shipments page created

---
+++ src/app/panel-de-control/envios/predeterminados/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'TrackApp - Envios predeterminados'
+}
+
+const CustomShipmentsPage = () => {
+  return <div>CustomShipmentsPage</div>
+}
+
+export default CustomShipmentsPage
