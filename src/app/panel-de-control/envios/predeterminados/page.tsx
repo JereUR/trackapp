237554ff15c4c3f40dc9f51626629a7a71a8c78a
JereUR <jeremias.jdv@gmail.com>
2024-07-03 03:06:32 -0300

    feat: Custom shipments page added

--- src/app/panel-de-control/envios/predeterminados/page.tsx
+++ src/app/panel-de-control/envios/predeterminados/page.tsx
@@ -1,11 +1,13 @@
 import { Metadata } from 'next'
 
+import CustomShipmentsList from '@/components/dashboard/shipments/custom/CustomShipmentsList'
+
 export const metadata: Metadata = {
   title: 'TrackApp - Envios predeterminados'
 }
 
 const CustomShipmentsPage = () => {
-  return <div>CustomShipmentsPage</div>
+  return <CustomShipmentsList />
 }
 
 export default CustomShipmentsPage
