691f9bde870ad46225f190e883efb16b869e9985
JereUR <jeremias.jdv@gmail.com>
2024-07-02 22:03:52 -0300

    feat: Show shipment details and redirect to edit or delte item function added

--- src/components/dashboard/shipments/Calendar.tsx
+++ src/components/dashboard/shipments/Calendar.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { ShipmentGroup, times } from '@/components/types/Shipment'
+import React, { useState } from 'react'
+import { ShipmentGroup, times, ShipmentItem } from '@/components/types/Shipment'
+import { Cross1Icon } from '@radix-ui/react-icons'
+import ModalShipment from './ModalShipment'
 
 interface CalendarProps {
   shipmentGroup: ShipmentGroup
@@ -12,14 +14,33 @@ const Calendar: React.FC<CalendarProps> = ({ shipmentGroup }) => {
   }
 
   const timeSlotHeight = 40 // Altura deseada para cada slot de tiempo
+  const [activeShipment, setActiveShipment] = useState<ShipmentItem | null>(
+    null
+  )
+  const [showConfirmDelete, setShowConfirmDelete] = useState<boolean>(false)
+
+  const handleShipmentClick = (shipment: ShipmentItem) => {
+    setActiveShipment(shipment)
+  }
+
+  const closeModal = () => {
+    setActiveShipment(null)
+    setShowConfirmDelete(false)
+  }
+
+  const handleDelete = () => {
+    // Lógica para borrar el envío
+    console.log('Deleting shipment:', activeShipment?.name)
+    closeModal()
+  }
 
   return (
-    <div className="w-full ml-8 mr-12 p-4 space-y-8">
-      <div className="border border-gray-500 p-4 rounded-lg shadow-lg bg-card">
-        <h3 className="text-xl font-semibold mb-4 text-foreground">
+    <div className="w-full ml-8 mr-12 p-4">
+      <div className="border border-gray-300 dark:border-gray-500 p-4 rounded-lg shadow-lg bg-card">
+        <h3 className="text-xl font-semibold mb-4 text-gray-500 dark:text-white">
           {shipmentGroup.date}
         </h3>
-        <div className="flex border-b border-gray-500 mb-2">
+        <div className="flex border-b border-gray-300 dark:border-gray-500 mb-2">
           {times.map((time, index) => (
             <div
               key={time}
@@ -27,7 +48,7 @@ const Calendar: React.FC<CalendarProps> = ({ shipmentGroup }) => {
               style={{ height: `${timeSlotHeight}px` }}
             >
               <div
-                className={`text-sm font-semibold text-foreground ${
+                className={`text-sm font-semibold text-gray-500 dark:text-white ${
                   index === 12 ? 'mr-6' : ''
                 }${index === 13 ? 'ml-6' : ''}`}
               >
@@ -69,32 +90,28 @@ const Calendar: React.FC<CalendarProps> = ({ shipmentGroup }) => {
                   <div
                     key={time}
                     className={`flex-1 text-center py-2 border ${
-                      isInTimeRange ? fleetColor : 'border-gray-500'
+                      isInTimeRange
+                        ? fleetColor
+                        : 'border-gray-300 dark:border-gray-500'
                     }`}
                     style={{
                       marginRight: marginRight,
                       marginLeft: marginLeft,
-                      height: `${timeSlotHeight}px` // Establecer altura del contenedor
+                      height: `${timeSlotHeight}px`
                     }}
                   ></div>
                 )
               })}
               {span > 0 && (
                 <div
-                  className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center"
+                  className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center cursor-pointer"
                   style={{
-                    left: `${Math.max(
-                      (startIndex + span / 2) * (100 / times.length) -
-                        (span / 2) * (100 / times.length),
-                      0
-                    )}%`,
-                    width: `${Math.min(
-                      span * (100 / times.length),
-                      100 - (startIndex + span / 2) * (100 / times.length)
-                    )}%`
+                    left: `${(startIndex * 100) / times.length}%`,
+                    width: `${(span * 100) / times.length}%`
                   }}
+                  onClick={() => handleShipmentClick(shipment)}
                 >
-                  <p className="text-card font-medium p-1 bg-gray-200 bg-opacity-75 rounded">
+                  <p className="text-[15px] text-gray-900 font-medium p-1 bg-gray-200 bg-opacity-75 rounded">
                     {shipment.name}
                   </p>
                 </div>
@@ -103,6 +120,13 @@ const Calendar: React.FC<CalendarProps> = ({ shipmentGroup }) => {
           )
         })}
       </div>
+      <ModalShipment
+        activeShipment={activeShipment}
+        closeModal={closeModal}
+        showConfirmDelete={showConfirmDelete}
+        setShowConfirmDelete={setShowConfirmDelete}
+        handleDelete={handleDelete}
+      />
     </div>
   )
 }
---
+++ src/components/dashboard/shipments/ModalShipment.tsx
@@ -0,0 +1,116 @@
+import { ShipmentItem } from '@/components/types/Shipment'
+import { Cross1Icon } from '@radix-ui/react-icons'
+import { useRouter } from 'next/navigation'
+import React, { Dispatch, SetStateAction } from 'react'
+
+interface Props {
+  activeShipment: ShipmentItem | null
+  closeModal: () => void
+  showConfirmDelete: boolean
+  setShowConfirmDelete: Dispatch<SetStateAction<boolean>>
+  handleDelete: () => void
+}
+
+const ModalShipment: React.FC<Props> = ({
+  activeShipment,
+  closeModal,
+  showConfirmDelete,
+  setShowConfirmDelete,
+  handleDelete
+}) => {
+  const router = useRouter()
+
+  return (
+    <div>
+      {activeShipment && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="bg-card text-foreground p-6 rounded-lg shadow-lg w-3/4 md:w-1/2 max-h-screen overflow-y-auto">
+            <div className="flex justify-between items-center mb-4">
+              <h4 className="text-xl font-semibold">Detalles de envío</h4>
+              <button onClick={closeModal} className="text-red-500">
+                <Cross1Icon className="h-5 w-5" />
+              </button>
+            </div>
+            <p>
+              <strong>Conductor asignado:</strong>{' '}
+              {activeShipment.assigned_driver.first_name}{' '}
+              {activeShipment.assigned_driver.last_name}
+            </p>
+            <div>
+              <strong>Puntos de entrega:</strong>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
+                {activeShipment.delivery_points.map((point, index) => (
+                  <div
+                    key={index}
+                    className="bg-gray-300 dark:bg-gray-700 text-foreground p-4 rounded-lg shadow"
+                  >
+                    <p>
+                      <strong>Nombre:</strong> {point.name}
+                    </p>
+                    <p>
+                      <strong>Carga:</strong>
+                    </p>
+                    <ul className="list-disc list-inside">
+                      {point.cargo.map((item, idx) => (
+                        <li key={idx}>
+                          {item.quantity} x {item.product}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex justify-end space-x-4 mt-4">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={() =>
+                  router.push(
+                    `/panel-de-control/envios/editar/${activeShipment.id}`
+                  )
+                }
+              >
+                Editar
+              </button>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded"
+                onClick={() => setShowConfirmDelete(true)}
+              >
+                Borrar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {showConfirmDelete && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="bg-card text-foreground p-6 rounded-lg shadow-lg w-1/3">
+            <h4 className="text-lg font-semibold mb-4">
+              Confirmar eliminación
+            </h4>
+            <p>
+              ¿Estás seguro de que deseas eliminar el envío{' '}
+              {`'${activeShipment?.name}'`}?
+            </p>
+            <div className="flex justify-end space-x-4 mt-4">
+              <button
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+                onClick={() => setShowConfirmDelete(false)}
+              >
+                Cancelar
+              </button>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded"
+                onClick={handleDelete}
+              >
+                Confirmar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ModalShipment
