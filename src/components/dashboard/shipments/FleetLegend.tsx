7eb088783e857fbc2d1f5ed49fd4da2b691aa67a
JereUR <jeremias.jdv@gmail.com>
2024-07-04 21:45:08 -0300

    feat: Input setted to radio button

--- src/components/dashboard/shipments/FleetLegend.tsx
+++ src/components/dashboard/shipments/FleetLegend.tsx
@@ -1,17 +1,20 @@
+import React from 'react'
+
 const FleetLegend: React.FC<{
   fleets: { id: number; name: string }[]
-  selectedFleets: number[]
-  onFleetSelection: (fleetId: number) => void
-}> = ({ fleets, selectedFleets, onFleetSelection }) => {
+  selectedFleet: number | null
+  onFleetSelection: (fleetId: number | null) => void
+}> = ({ fleets, selectedFleet, onFleetSelection }) => {
   const fleetColors: Record<number, string> = {
     1: 'bg-red-500',
     2: 'bg-green-500'
+    // Agrega aquí más colores según tus flotas
   }
 
   return (
-    <div className="m-4">
-      <h3 className="text-lg font-semibold mb-2">
-        Índice de colores por flota
+    <div className="md:m-4">
+      <h3 className="text-lg font-semibold mb-2 text-center md:text-left">
+        Indique la flota
       </h3>
       <div className="flex gap-4 items-center ml-4">
         {fleets.map((fleet) => (
@@ -23,10 +26,12 @@ const FleetLegend: React.FC<{
             />
             <span>{fleet.name}</span>
             <input
-              type="checkbox"
+              type="radio"
               className="ml-2 cursor-pointer"
-              checked={selectedFleets.includes(fleet.id)}
-              onChange={() => onFleetSelection(fleet.id)}
+              checked={selectedFleet === fleet.id}
+              onChange={() =>
+                onFleetSelection(selectedFleet === fleet.id ? null : fleet.id)
+              }
             />
           </div>
         ))}
