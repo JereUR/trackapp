d19e6e9fb294ffa3e4682ef318f2686e2c31d26f
JereUR <jeremias.jdv@gmail.com>
2024-07-05 02:29:40 -0300

    feat: Add user page added

---
+++ src/app/panel-de-control/usuarios/agregar/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'TrackApp - Agregar usuario'
+}
+
+const AddUsersPage = () => {
+  return (
+    <div className="m-4 md:m-10">
+      User form
+    </div>
+  )
+}
+
+export default AddUsersPage
