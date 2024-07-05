fc2dc671a1419a2dbed738c1b0df0c8e0cac426e
JereUR <jeremias.jdv@gmail.com>
2024-07-05 02:35:10 -0300

    feat: Wrapped users table on Suspense component

--- src/app/panel-de-control/usuarios/page.tsx
+++ src/app/panel-de-control/usuarios/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 
 import UsersTable from '@/components/dashboard/users/UsersTable'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'TrackApp - Usuarios'
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 const UsersPage = () => {
   return (
     <div className="m-4 md:m-10">
-      <UsersTable />
+      <Suspense fallback={<div>Loading...</div>}>
+        <UsersTable />
+      </Suspense>
     </div>
   )
 }
