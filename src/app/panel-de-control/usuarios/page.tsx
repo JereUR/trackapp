25f815d085797faa6c1f1a2e3e13c30defe86d41
JereUR <jeremias.jdv@gmail.com>
2024-07-05 02:29:27 -0300

    feat: Users page updated

--- src/app/panel-de-control/usuarios/page.tsx
+++ src/app/panel-de-control/usuarios/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { Metadata } from 'next'
+
+import UsersTable from '@/components/dashboard/users/UsersTable'
+
+export const metadata: Metadata = {
+  title: 'TrackApp - Usuarios'
+}
 
 const UsersPage = () => {
-  return <div>UsersPage</div>
+  return (
+    <div className="m-4 md:m-10">
+      <UsersTable />
+    </div>
+  )
 }
 
 export default UsersPage
